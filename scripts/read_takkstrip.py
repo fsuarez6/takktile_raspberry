50d285e18ceb8f59cb7bf7f48982f598266637f4
fsuarez6 <fsuarez6@gmail.com>
2013-11-07 21:09:46 +0100

    Getting the pressure and temp properly. Missing generalization of the code

--- scripts/read_takkstrip.py
+++ scripts/read_takkstrip.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 import smbus
-import time
+import time, os
 import rospy
 
 '''
@@ -15,11 +15,17 @@ class I2CInterface():
   max_strips = 8
   max_sensors = 5
   # Initialise interface
-  def __init__(self, pi_version = 1, address_type='old'):
+  def __init__(self, pi_version = 1):
     self.bus = smbus.SMBus(pi_version)
-    self.sensors_addr = self.get_addresses()
-    rospy.logdebug('Sensors address: %s' % str(self.sensors_addr))
-    self.get_coefficients(self.sensors_addr)
+    self.sensors = self.get_addresses()
+    rospy.logdebug('Sensors address: %s' % str(self.sensors))
+    self.coefficients = self.get_coefficients(self.sensors)
+    while not rospy.is_shutdown():
+      print '-----------'
+      self.start_conversions()
+      for sensor, coefficients in self.coefficients.items():
+        print self.get_pressure_temperature(sensor, coefficients)
+      os.system(['clear','cls'][os.name == 'nt'])
   
   def get_addresses(self):
     addresses = dict()
@@ -38,20 +44,15 @@ class I2CInterface():
   def get_coefficients(self, sensors):
     coefficients = dict()
     for strip in sensors.keys():
-      coefficients[strip] = []
       for sensor in sensors[strip]:
         # Select sensor
         self.bus.write_quick(sensor)
         try:
           # Request coefficients
           block = self.bus.read_i2c_block_data(self.attiny_addr, 0x04)[:8]
-          data = []
-          data.append((block[0] << 8) | block[1])
-          data.append((block[2] << 8) | block[3]) 
-          data.append((block[4] << 8) | block[5])
-          data.append((block[6] << 8) | (block[7] >> 2))
           short_values = []
-          for value in data:
+          for i in xrange(0, len(block), 2):
+            value = (block[i] << 8) | block[i+1]
             if(value >= 2**15):
               value -= 2**16
             short_values.append(float(value))
@@ -60,12 +61,45 @@ class I2CInterface():
           short_values[2] /= 16384.0
           short_values[3] /= 4194304.0
           # Append the sensor coefficients
-          coefficients[strip].append(short_values)
+          coefficients[sensor] = short_values
         except:
           rospy.logerr('Failed to get the coefficients from %d:%s' % (strip, hex(sensor)))
         # Turn off sensor
         self.bus.read_byte(sensor)
     return coefficients
+  
+  def start_conversions(self):
+    # Turn on all the sensors
+    self.bus.write_quick(self.sensor_all_on)
+    # Start data conversion
+    self.bus.write_i2c_block_data(self.attiny_addr, 0x12, [0x01])
+    self.bus.read_byte(self.sensor_all_on)
+    time.sleep(2e-3)
+  
+  def get_pressure_temperature(self, sensor, coefficients):
+    # Select sensor
+    self.bus.write_quick(sensor)
+    # Request P/T data
+    try:
+      block = self.bus.read_i2c_block_data(self.attiny_addr, 0x00)[:4]
+    except:
+      return (0,0)
+    # Turn off sensor
+    self.bus.read_byte(sensor)
+    # Calculate pressure & temperature
+    data = []
+    for i in xrange(0, len(block), 2):
+      value = (block[i] << 8) | block[i+1]
+      if(value >= 2**15):
+        value -= 2**16
+      data.append(value)
+    pressure = float(data[0]) / 64.0
+    temp = float(data[1]) / 64.0
+    [a0, b1, b2, c12] = coefficients
+    pressure_comp = a0 + (b1 + c12 * temp) * pressure + b2 * temp
+    pressure_kpa = ((65.0 / 1023.0) * pressure_comp) + 50.0        # kPa
+    temp_celsius = -(temp - 498.0) / 5.35 + 25.0                   # C
+    return pressure_comp, temp_celsius
   
 if __name__ == '__main__':
   # TODO: Validate inputs
